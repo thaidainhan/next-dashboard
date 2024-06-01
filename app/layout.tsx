e2a4a234c3eb7a27adcaa1ea96b43477b08efbae
Thai Dai Nhan <>
2024-06-01 14:46:35 +0700

    init next-dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const FONT_CSS = `${inter.className} antialiased`;
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={FONT_CSS}>{children}</body>
     </html>
   );
 }
